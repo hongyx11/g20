fbc2a39c0b6aa2609b4fcdccd31e385144f50538
omairyrm <rabab.omairy@kaust.edu.sa>
2020-12-07 11:36:47 +0300

    gbr

---
+++ install-anaconda.sh
@@ -0,0 +1,4 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+bash Anaconda3-2020.07-Linux-x86_64.sh -b -p $HOME/rababsources/anaconda3
+export PATH=$HOME/rababsources/anaconda3/bin:$PATH
+rm Anaconda3-2020.07-Linux-x86_64.sh
